cc36474f9db1ef926317912cce5c8fa7727892d3
Beinan Li <li.beinan@gmail.com>
2023-12-12 14:08:45 +0800

    'fixed folder retriever; added output reflection support'

--- demo/form.py
+++ demo/form.py
@@ -54,10 +54,10 @@ class Controller(ui.FormController):
                 return
         self.stop_progress()
         out = vars(self.get_latest_model())
-        dmp = json.dumps(out, indent=2)
+        self.model['export'] = [osp.join(util.get_platform_tmp_dir(), 'form.out.json')]
+        util.save_json(self.model['export'][0], out)
+        dmp = json.dumps(self.model, indent=2)
         self.prompt.info(f'{dmp}', confirm=True)
-        util.save_json(exported := osp.join(util.get_platform_tmp_dir(), 'form.out.json'), out)
-        self.model['export'] = copy.deepcopy([exported])
         self.update_view()
 
 
@@ -78,13 +78,10 @@ def main():
     weight_wgt = ui.FloatEntry(pg1, 'weight', "Weight (kg)", 51, "float widget", True, (50.2, 70.3), 0.1, 1)
     gender_wgt = ui.SingleOptionEntry(pg1, 'gender', "Gender", ["Male", "Female", "[Secret]"], "Female", "option widget")
     protagonist_wgt = ui.BoolEntry(pg1, 'is_protagonist', "Protagonist", True, "checkbox widget")
-    bio_widget = ui.TextEntry(pg1, 'bio', "Bio", """Robin Sena (瀬名 ロビン, Sena Robin) is a soft-spoken 15-year-old Hunter and craft-user with pyrokinetic abilities. She was raised in a convent in Italy-(where she was taught how to use and control her craft '
-                                   'in "
-                                          "hunting down Witches) before she was sent to the STN-J to gather information for the Solomon administration; even though she was born in Japan, she had moved to Tuscany when she was still very young. Her witch powers allow her to channel her energy into shields capable of blocking solid matter and crafts, magical powers. However, any use of her power temporarily weakens her eyesight, greatly reducing her accuracy and effectiveness. This problem is solved when she begins to wear glasses. Although she is good-natured, Robin employs her gift with lethal force when necessary to save a life or for the good of others. As the series progresses, her powers increase rapidly until she is labeled dangerous by Solomon and is ordered to be hunted as a witch. It is discovered that Robin is a "Designer Witch" and was created through "Project Robin", a genetic engineering project. Her mother, Maria, had agreed to genetic manipulation and called the unborn Robin "Hope." Robin was designed to give birth to witch kind, what was once called "divinities" in ancient history. Robin was given thousands of years witch kind's memories, the origin of "craft". This enables her to understand the sadness arising from the conflict between humans and witches, in turn allowing her to find a way for humans and witches to peacefully coexist. Amon volunteers to be a watchman who will terminate her if she becomes destructive. She accepts this. After the collapse of the Factory, her fate is unknown. The reactions of the other characters show that she is believed to be alive, but is said to be dead for her own safety. However, after the ending credits it shows that a new hunter arrived at the STN-J, which is actually not a new hunter but Robin herself. She is voiced by Akeno Watanabe in Japanese and Kari Wahlgren in English. -- Wikipedia""",
-                              'text widget.')
-
+    bio_widget = ui.TextEntry(pg1, 'bio', "Bio", """Robin Sena (瀬名 ロビン, Sena Robin) is a soft-spoken 15-year-old Hunter and craft-user with pyrokinetic abilities. She was raised in a convent in Italy-(where she was taught how to use and control her
+    craft in hunting down Witches) before she was sent to the STN-J to gather information for the Solomon administration; even though she was born in Japan, she had moved to Tuscany when she was still very young. Her witch powers allow her to channel her energy into shields capable of blocking solid matter and crafts, magical powers. -- Wikipedia""", 'text widget.')
     occupation_wgt = ui.MultiOptionEntry(pg2, 'occupation', 'Occupation', ['Lead', 'Warrior', 'Wizard', 'Detective', 'Hacker', 'Clerk'], ['Wizard', 'Detective'], "option widget")
-    export_wgt = ui.FileEntry(pg3, 'export', '', '', 'Path to export file', False, [('JSON', '*.json'), ('All Files', '*.*')])
+    export_wgt = ui.FileEntry(pg3, 'export', '', '', 'Path to exported file', False, [('JSON', '*.json'), ('All Files', '*.*')])
     action_bar = ui.FormActionBar(ui.Globals.root, ctrlr)
     progress_bar = ui.ProgressBar(ui.Globals.root, ui.Globals.progressQueue)
     progress_bar.poll()
